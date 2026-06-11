89c15b4197fd90961c42a65e6265f34d1bc36574
anhtai26 <anhtaipxuan@gmail.com>
2026-06-11 11:29:46 +0700

    Fix: Fix lai upload len folder tren cloudinary

--- backend/src/controller/admin.controller.js
+++ backend/src/controller/admin.controller.js
@@ -5,7 +5,8 @@ import cloudinary from '../lib/cloudinary.js'
 const uploadToCloudinary = async (file) => {
   try {
     const result = await cloudinary.uploader.upload(file.tempFilePath, {
-      resource_type: 'auto'
+      resource_type: 'auto',
+      folder: 'My_spotify'
     })
     return result.secure_url
   } catch (error) {

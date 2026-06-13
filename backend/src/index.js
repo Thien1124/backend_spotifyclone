89296a6afed8787d844f55da51bf65f52c5c8940
anhtai26 <anhtaipxuan@gmail.com>
2026-06-13 22:41:32 +0700

    Fix: Sua file index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,67 +1,98 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import { connectDB } from './lib/db.js';
-import { clerkMiddleware } from '@clerk/express';
-import fileUpload from 'express-fileupload';
-import path from 'path';
+import express from 'express'
+import dotenv from 'dotenv'
+import { connectDB } from './lib/db.js'
+import { clerkMiddleware } from '@clerk/express'
+import fileUpload from 'express-fileupload'
+import path from 'path'
 
-import userRoutes from './routes/user.route.js';
-import authRoutes from './routes/auth.route.js';
-import adminRoutes from './routes/admin.route.js';
-import songRoutes from './routes/song.route.js';
-import albumRoutes from './routes/album.route.js';
-import statsRoutes from './routes/stat.route.js';
-import { createServer } from 'http';
-import { initializeSocket } from './lib/socket.js';
-import cors from 'cors';
+import userRoutes from './routes/user.route.js'
+import authRoutes from './routes/auth.route.js'
+import adminRoutes from './routes/admin.route.js'
+import songRoutes from './routes/song.route.js'
+import albumRoutes from './routes/album.route.js'
+import statsRoutes from './routes/stat.route.js'
+import { createServer } from 'http'
+import { initializeSocket } from './lib/socket.js'
+import cors from 'cors'
 import dns from 'node:dns'
+import cron from 'node-cron'
+import fs from 'fs'
 
 dns.setServers(['1.1.1.1', '1.0.0.1'])
 
-dotenv.config();
+dotenv.config()
 
-const app = express();
-const __dirname = path.resolve();
-const PORT = process.env.PORT || 3000;
+const app = express()
+const __dirname = path.resolve()
+const PORT = process.env.PORT || 3000
 
-const httpServer = createServer(app); // Tạo HTTP server từ Express app
-initializeSocket(httpServer); // Khởi tạo Socket
+const httpServer = createServer(app) // Tạo HTTP server từ Express app
+initializeSocket(httpServer) // Khởi tạo Socket
 
 app.use(
-	cors({
-		origin: "http://localhost:3000",
-		credentials: true,
-	})
-);
+  cors({
+    origin: 'http://localhost:3000',
+    credentials: true
+  })
+)
 
-app.use(express.json()); // to parse req.body
-app.use(clerkMiddleware()); // this will add auth to req obj => req.auth
+app.use(express.json()) // to parse req.body
+app.use(clerkMiddleware()) // this will add auth to req obj => req.auth
 app.use(
-	fileUpload({
-		useTempFiles: true,
-		tempFileDir: path.join(__dirname, "tmp"),
-		createParentPath: true,
-		limits: {
-			fileSize: 10 * 1024 * 1024, // 10MB  max file size
-		},
-	})
-);
+  fileUpload({
+    useTempFiles: true,
+    tempFileDir: path.join(__dirname, 'tmp'),
+    createParentPath: true,
+    limits: {
+      fileSize: 10 * 1024 * 1024 // 10MB  max file size
+    }
+  })
+)
 
-app.use("/api/users", userRoutes);
-app.use("/api/auth", authRoutes);
-app.use("/api/admin", adminRoutes);
-app.use("/api/songs", songRoutes);
-app.use("/api/albums", albumRoutes);
-app.use("/api/stats", statsRoutes);
+// cron jobs
+const tempDir = path.join(process.cwd(), "tmp")
+cron.schedule('0 * * * *', () => {
+	if (fs.existsSync(tempDir)) {
+    fs.readdir(tempDir, (err, files) => {
+      if (err) {
+        console.log('error', err)
+        return
+      }
+      for (const file of files) {
+        fs.unlink(path.join(tempDir, file), (err) => {})
+      }
+    })
+  }
+})
+
+app.use('/api/users', userRoutes)
+app.use('/api/auth', authRoutes)
+app.use('/api/admin', adminRoutes)
+app.use('/api/songs', songRoutes)
+app.use('/api/albums', albumRoutes)
+app.use('/api/stats', statsRoutes)
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../frontend/dist')))
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../frontend', 'dist', 'index.html'))
+  })
+}
 
 //error handle
 app.use((err, req, res, next) => {
-    res.status(500).json({message: process.env.NODE_ENV === "production" ? err.message : "Internal Server Error"});
+  res
+    .status(500)
+    .json({
+      message:
+        process.env.NODE_ENV === 'production'
+          ? err.message
+          : 'Internal Server Error'
+    })
 })
 httpServer.listen(PORT, () => {
-  console.log('Server is running on port ' + PORT);
-  connectDB();
-});
+  console.log('Server is running on port ' + PORT)
+  connectDB()
+})
 
 // socket.io
-
